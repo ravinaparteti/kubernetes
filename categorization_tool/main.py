850255e3ce67f11f7d668c6a8d8c920e3dc6cc3e
Kunal <kunal.ghatode@tudip.com>
2025-02-20 22:48:50 +0530

    commit

--- categorization_tool/main.py
+++ categorization_tool/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Kubernetes! Hi, From Kunal!!! Deploy using CICD"
+    return "Hello, Kubernetes!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000)
