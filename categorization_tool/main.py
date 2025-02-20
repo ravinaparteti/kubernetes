ad9929683cfa28ffa7f4014964ea2f77ca1784b0
Kunal <kunal.ghatode@tudip.com>
2025-02-20 20:41:01 +0530

    Kubernetes deployment

--- categorization_tool/main.py
+++ categorization_tool/main.py
@@ -1,2 +1,10 @@
-def hello_world(request):
-    return "final test with Kubernetes with configMap"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Kubernetes!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)
