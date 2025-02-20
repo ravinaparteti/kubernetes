ada327a82122dc8af9413b99e2f3b32453afb0f5
Kunal <kunal.ghatode@tudip.com>
2025-02-20 21:19:16 +0530

    Kubernetes deployment

--- categorization_tool/main.py
+++ categorization_tool/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Kubernetes!"
+    return "Hello, Kubernetes! Hi, From Kunal!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000)
