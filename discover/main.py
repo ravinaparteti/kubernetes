347a3774ac7edd02579e108655273ba0f81216b7
Kunal <kunal.ghatode@tudip.com>
2025-02-21 14:24:31 +0530

    commit

--- discover/main.py
+++ discover/main.py
@@ -1,2 +1,10 @@
-def hello_world(request):
-    return "final diagnostic with 1 GB with mongo url secret manager"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Discover"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)
